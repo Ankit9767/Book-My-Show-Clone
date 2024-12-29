654b456b25a5f931a01f4e3ad84d47570489a68d
Ankit9767 <anktmb05@gmail.com>
2024-12-29 13:58:31 +0530

    changes in cinema seat repository

--- BookMyShow/src/main/java/com/example/BookMyShow/Repository/CinemaSeatRepository.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Repository/CinemaSeatRepository.java
@@ -1,5 +1,7 @@
 package com.example.BookMyShow.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.example.BookMyShow.Domain.CinemaSeat;
 
 @Repository
 public interface CinemaSeatRepository extends JpaRepository<CinemaSeat, Long> {
+	
+	List<CinemaSeat> findByCinemaHall_CinemaHallID(Integer cinemaHallID);
+	
+	void deleteByCinemaHall_CinemaHallID(Integer cinemaHallID);
 }
 
