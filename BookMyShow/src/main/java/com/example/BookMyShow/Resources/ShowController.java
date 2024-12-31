2d46c0d48f13825784ec383d461b5c4145aa6c7a
Ankit9767 <anktmb05@gmail.com>
2024-12-31 14:31:49 +0530

    updates in show controller

--- BookMyShow/src/main/java/com/example/BookMyShow/Resources/ShowController.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Resources/ShowController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BookMyShow.Domain.Show;
+import com.example.BookMyShow.Domain.ShowSeat;
 import com.example.BookMyShow.Service.ShowService;
 
 @RestController
@@ -43,6 +44,40 @@ public class ShowController {
     @PutMapping("/update/{id}")
     public Show updateShow(@PathVariable Long id, @RequestBody Show updatedShow) {
         return showService.updateShow(id, updatedShow);
+    }
+    
+    @GetMapping("/cinema-Hall/{cinemaHallID}")
+    public ResponseEntity<List<Show>> getShowsByCinemaHallId(@PathVariable Integer cinemaHallID) {
+        List<Show> shows = showService.getShowsByCinemaHallId(cinemaHallID);
+        if (shows.isEmpty()) {
+            System.out.println("No shows found for cinema hall ID: " + cinemaHallID);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(shows);
+    }
+    
+    @GetMapping("/{showID}/seats")
+    public ResponseEntity<List<ShowSeat>> getSeatsByShowId(@PathVariable Long showID) {
+        List<ShowSeat> seats = showService.getSeatsByShowId(showID);
+        return ResponseEntity.ok(seats);
+    }
+    
+    
+    @GetMapping("/movie/{movieID}")
+    public ResponseEntity<List<Show>> getShowsByMovieId(@PathVariable Long movieID) {
+        List<Show> shows = showService.getShowsByMovieId(movieID);
+        if (shows.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(shows);
+    }
+    
+    @GetMapping("/cinema-Hall/{cinemaHallID}/movie/{movieID}")
+    public ResponseEntity<List<Show>> getShowsByCinemaHallAndMovie(
+        @PathVariable Integer cinemaHallID,
+        @PathVariable Long movieID) {
+        List<Show> shows = showService.getShowsByCinemaHallAndMovie(cinemaHallID, movieID);
+        return shows.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(shows);
     }
 }
 
