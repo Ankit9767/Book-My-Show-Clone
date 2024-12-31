99864928c1663830675bc515d4d21b30cc566e28
Ankit9767 <anktmb05@gmail.com>
2024-12-31 14:36:59 +0530

    updates in cinema controller

--- BookMyShow/src/main/java/com/example/BookMyShow/Resources/CinemaController.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Resources/CinemaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +32,19 @@ public class CinemaController {
         return ResponseEntity.ok(cinemaService.getAllCinemas());
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteCinema(@PathVariable Long id) {
         cinemaService.deleteCinema(id);
         return ResponseEntity.noContent().build();
+    }
+    
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Cinema> updateCinema(
+            @PathVariable Long id,
+            @RequestBody Cinema updatedCinema) {
+        Cinema cinema = cinemaService.updateCinema(id, updatedCinema);
+        return ResponseEntity.ok(cinema);
     }
 }
 
