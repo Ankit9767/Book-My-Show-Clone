2032f2e75d3ad9d0a6282eabfc3a75903e2cd1b9
Ankit9767 <anktmb05@gmail.com>
2024-12-31 14:36:20 +0530

    updates in cinema service

--- BookMyShow/src/main/java/com/example/BookMyShow/Service/CinemaService.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Service/CinemaService.java
@@ -6,12 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.BookMyShow.Domain.Cinema;
+import com.example.BookMyShow.Domain.CinemaHall;
+import com.example.BookMyShow.Repository.CinemaHallRepository;
 import com.example.BookMyShow.Repository.CinemaRepository;
+import com.example.BookMyShow.Repository.CinemaSeatRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CinemaService {
     @Autowired
     private CinemaRepository cinemaRepository;
+    
+    @Autowired
+    private CinemaHallRepository cinemaHallRepository ;
+    
+    @Autowired
+    private CinemaSeatRepository cinemaSeatRepository ;
 
     public Cinema createCinema(Cinema cinema) {
         return cinemaRepository.save(cinema);
@@ -21,8 +32,29 @@ public class CinemaService {
         return cinemaRepository.findAll();
     }
 
-    public void deleteCinema(Long id) {
-        cinemaRepository.deleteById(id);
+    
+    @Transactional
+    public void deleteCinema(Long cinemaID) {
+        
+        Integer cinemaIDInt = cinemaID.intValue(); 
+
+        List<CinemaHall> cinemaHalls = cinemaHallRepository.findByCinema_CinemaID(cinemaIDInt);
+
+        for (CinemaHall cinemaHall : cinemaHalls) {
+            cinemaSeatRepository.deleteByCinemaHall_CinemaHallID(cinemaHall.getCinemaHallID());
+        }
+        cinemaHallRepository.deleteAll(cinemaHalls);
+        cinemaRepository.deleteById(cinemaID); 
+    }
+    
+    
+    public Cinema updateCinema(Long id, Cinema updatedCinema) {
+        return cinemaRepository.findById(id).map(existingCinema -> {
+            existingCinema.setName(updatedCinema.getName());
+            existingCinema.setTotalCinemaHalls(updatedCinema.getTotalCinemaHalls());
+            existingCinema.setCity(updatedCinema.getCity());
+            return cinemaRepository.save(existingCinema);
+        }).orElseThrow(() -> new RuntimeException("Cinema not found with id " + id));
     }
 }
 
