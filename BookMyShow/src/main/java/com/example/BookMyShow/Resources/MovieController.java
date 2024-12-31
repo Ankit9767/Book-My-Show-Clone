229bc9257fc369307fd6806bf48bb4fe1f5fae1f
Ankit9767 <anktmb05@gmail.com>
2024-12-31 14:33:39 +0530

    updates in movie controller

--- BookMyShow/src/main/java/com/example/BookMyShow/Resources/MovieController.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Resources/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,19 @@ public class MovieController {
         movieService.deleteMovie(id);
         return ResponseEntity.noContent().build();
     }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Movie movie = movieService.getMovieById(id); 
+        return ResponseEntity.ok(movie);
+    }
+    
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        Movie updatedMovie = movieService.updateMovie(id, movie);
+        return ResponseEntity.ok(updatedMovie);
+    }
+    
 }
 
